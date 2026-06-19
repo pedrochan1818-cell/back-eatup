7cb175ba7fed3c43ae043468dd64fef78cc67986
pedroakechan <pedroakechan321@gmail.com>
2026-06-18 20:10:35 -0600

    cors para back produccion

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,9 +9,9 @@ class Cors
     {
         $response = $next($request);
         //local
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        //$response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         //web
-        //$response->headers->set('Access-Control-Allow-Origin', 'https://eatup-front.vercel.app');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://back-eatup.onrender.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
